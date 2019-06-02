bc6f0630cbb73728504113fc9ba9decbe54756dc
timrodenbroeker <post@timrodenbroeker.de>
2019-06-02 13:29:31 +0200

    Initialization

---
+++ lifeline01/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Ui></Ui>
+  </div>
+</template>
+
+<script>
+import Ui from './components/ui/ui.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Ui
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ lifeline01/src/components/ui/ui.vue
@@ -0,0 +1,9 @@
+// Define a new component called button-counter
+Vue.component('Ui', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
